da6bb051abcf2916372e484441ea089bce32b514
Wiktor Starzak <65298369+wstarzak@users.noreply.github.com>
2024-08-05 15:34:00 +0200

    Fixed algorithm, tuning steps varies depending on frequency

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -20,7 +20,7 @@
 #define SWR_REF_PIN A7
 #define SWR_FWD_PIN A0
 
-#define MAX_SWR 11.3
+#define MAX_SWR 1.3
 #define MAX_TUNING_SWR 15.0
 
 #define L_COUNT 7
@@ -28,21 +28,32 @@
 
 #define DEBUG true
 
+#define TUNING_STEP_160M 5
+#define TUNING_STEP_80M 4
+#define TUNING_STEP_60M 3
+#define TUNING_STEP_40M 3
+#define TUNING_STEP_30M 2
+#define TUNING_STEP_20M 2
+#define TUNING_STEP_18M 1
+#define TUNING_STEP_15M 1
+#define TUNING_STEP_12M 1
+#define TUNING_STEP_10M 1
+
 struct Point
 {
-  int ind;
-  int cap;
+  uint16_t ind;
+  int16_t cap;
 };
 
-Point grid_array[256];
+Point grid_array[128];
 Vector<Point> grid_vector(grid_array);
 
 // The registers needs to be cleared singe we have 8 output shift register
 boolean registers_l[8];
 boolean registers_c[8];
 
-int cap_value;
-int ind_value;
+int16_t cap_value;
+uint16_t ind_value;
 
 float frequency;
 bool frequency_lock = false;
@@ -86,7 +97,7 @@ void clearRegistersL()
 {
   for (int i = 0; i < L_COUNT; i++)
   {
-    registers_l[i] = LOW;
+    registers_l[i] = HIGH;
   }
   ind_value = 0;
 }
@@ -182,25 +193,27 @@ void coarse_ind(uint16_t ind_step = 3) // x 3 should be a variable dependent on 
   uint16_t max_ind_val = pow(2, L_COUNT);
   
   float min_swr = 99.9;
-  int min_swr_ind_val = ind_value;
+  uint16_t min_swr_ind_val = ind_value;
 
   for (uint16_t i = ind_step; i < max_ind_val; i += ind_step)
   {
     set_ind_value(i);
     writeRegistersL();
-    if (grid_vector.max_size() > grid_vector.size()) { grid_vector.push_back({ind_value, (-1+(2*c_side_trx))*cap_value}); }
+    if (grid_vector.max_size() > grid_vector.size()) { grid_vector.push_back({ind_value, (int16_t)(-1+(2*c_side_trx))*cap_value}); }
     float current_swr = getSWR();
     if(DEBUG) {
-      char s [128];
-      sprintf (s, "[C_IND]: SWR: %4.2f, IT: %2u, L: %3d, C: %3d", current_swr, i, ind_value, (-1+(2*c_side_trx))*cap_value);
+      char s [64];
+      char str_swr[5] = {0};
+      dtostrf(current_swr, 2, 2, str_swr);
+      sprintf (s, "[C_IND]: SWR: %s, IT: %2u, L: %3d, C: %3d", str_swr, i, ind_value, (int16_t)(-1+(2*c_side_trx))*cap_value);
       Serial.println(s);
     }
-    if(current_swr == 0.0) { break; } // TX stopped while tunnning
+    if(current_swr == 0.0) { tuned = false; return; } // TX stopped while tunnning
     if(current_swr < min_swr) {
       min_swr = current_swr;
       min_swr_ind_val = ind_value;
     }
-    if((current_swr - min_swr) > 8.0) { break; } // We do not want really tune if the swr increase instead of decrease...
+    if((current_swr - min_swr) > 1.0) { break; } // We do not want really tune if the swr increase instead of decrease...
   }
   set_ind_value(min_swr_ind_val);
   writeRegistersL();
@@ -217,61 +230,67 @@ void coarse_cap(uint16_t cap_step = 3) // x 8 should be a variable dependent on 
   {
     set_cap_value(i);
     writeRegistersC();
-    if (grid_vector.max_size() > grid_vector.size()) { grid_vector.push_back({ind_value, (-1+(2*c_side_trx))*cap_value}); }
+    if (grid_vector.max_size() > grid_vector.size()) { grid_vector.push_back({ind_value, (int16_t)(-1+(2*c_side_trx))*cap_value}); }
     float current_swr = getSWR();
     if(DEBUG) {
-      char s [128];
-      sprintf (s, "[C_CAP]: SWR: %4.2f, IT: %2u, L: %3d, C: %3d", current_swr, i, ind_value, (-1+(2*c_side_trx))*cap_value);
+      char s [64];
+      char str_swr[5] = {0};
+      dtostrf(current_swr, 2, 2, str_swr);
+      sprintf (s, "[C_CAP]: SWR: %s, IT: %2u, L: %3d, C: %3d", str_swr, i, ind_value, (-1+(2*c_side_trx))*cap_value);
       Serial.println(s);
     }
-    if(current_swr == 0.0) { break; } // TX stopped while tunnning
+    if(current_swr == 0.0) { tuned = false; return; } // TX stopped while tunnning
     if(current_swr < min_swr) {
       min_swr = current_swr;
       min_swr_cap_val = cap_value;
     }
-    if((current_swr - min_swr) > 4.0) { break; } // We do not want really tune if the swr increase instead of decrease...
+    if((current_swr - min_swr) > 1.0) { break; } // We do not want really tune if the swr increase instead of decrease...
   }
   set_cap_value(min_swr_cap_val);
   writeRegistersC();
 }
 
 void fine_tuning(uint16_t fine_step = 1) {
-  int init_int_val = ind_value;
-  int init_cap_val = cap_value;
+  uint16_t init_ind_val = ind_value;
+  int16_t init_cap_val = cap_value;
 
   float min_swr = 99.9;
-  int min_swr_cap_val = cap_value;
-  int min_swr_ind_val = ind_value;
-  for(int8_t ind = -fine_step; ind < 2; ind+=fine_step) {
-    for(int8_t cap = -fine_step; cap < 2; cap+=fine_step) {
+  int16_t min_swr_cap_val = cap_value;
+  uint16_t min_swr_ind_val = ind_value;
+  for(uint16_t ind = -fine_step; ind < 2; ind+=fine_step) {
+    for(int16_t cap = -fine_step; cap < 2; cap+=fine_step) {
       bool in_grid = false;
-      if(((init_int_val + ind) > (pow(2, L_COUNT) - 1)) || ((init_int_val + ind) < 0)) { continue; }
+      if(((init_ind_val + ind) > (pow(2, L_COUNT) - 1)) || ((init_ind_val + ind) < 0)) { continue; }
       if(((init_cap_val + cap) > (pow(2, C_COUNT) - 1)) || ((init_cap_val + cap) < 0)) { continue; }
       for (size_t i = 0; i < grid_vector.max_size(); i++)
       {
-        if ((grid_vector.at(i).ind == (init_int_val + ind)) && (grid_vector.at(i).cap == ((-1+(2*c_side_trx))*(init_cap_val + cap))))
+        if ((grid_vector.at(i).ind == (init_ind_val + ind)) && (grid_vector.at(i).cap == ((-1+(2*c_side_trx))*(init_cap_val + cap))))
         {
           in_grid = true;
           break;
         }
         else
         {
-          if (grid_vector.max_size() > grid_vector.size()) { grid_vector.push_back({(init_int_val + ind), (-1+(2*c_side_trx))*(init_cap_val + cap)}); }
+          if (grid_vector.max_size() > grid_vector.size()) { grid_vector.push_back({(init_ind_val + ind), (int16_t)(-1+(2*c_side_trx))*(init_cap_val + cap)}); }
         }
       }
       if(in_grid) { continue; }
-      set_ind_value(init_int_val + ind);
+      set_ind_value(init_ind_val + ind);
       writeRegistersL();
       set_cap_value(init_cap_val + cap);
       writeRegistersC();
       float current_swr = getSWR();
+      if(current_swr == 0.0) { tuned = false; return; }
       if(DEBUG) {
-        char s [128];
-        sprintf (s, "[FINE]: SWR: %4.2f, L: %3d, C: %3d", current_swr, ind_value, (-1+(2*c_side_trx))*cap_value);
+        tuned = false;
+        char s [64];
+        char str_swr[5] = {0};
+        dtostrf(current_swr, 2, 2, str_swr);
+        sprintf (s, "[FINE]: SWR: %s, L: %3d, C: %3d", str_swr, ind_value, (-1+(2*c_side_trx))*cap_value);
         Serial.println(s);
       }
       if(current_swr < MAX_SWR) { tuned = true; return; }
-      if(current_swr == 0.0) { break; } // TX stopped while tunnning
+      if(current_swr == 0.0) { tuned = false; break; } // TX stopped while tunnning
       if(current_swr < min_swr) {
         min_swr = current_swr;
         min_swr_cap_val = cap_value;
@@ -297,8 +316,15 @@ void reset_LC() {
 void loop()
 {
   float freq_lock[10];
-  for(int i=0; i<10; i++) {
-    while(!FreqCount.available()) { continue; }
+  frequency_lock = false;
+  uint16_t tuning_step = 1;
+
+  for(int i = 0; i<10; i++) {
+    while(!FreqCount.available()) { if(getSWR() == 0.0) break; else continue; }
+    if(getSWR() == 0.0) {
+      frequency_lock = false;
+      break;
+    }
     frequency = (((float)FreqCount.read() * 4096.0 / 100000.0));
     freq_lock[i] = frequency;
     if(i > 3) {
@@ -317,33 +343,76 @@ void loop()
   }
 
   if(frequency_lock) {
+    switch ((int)floor(frequency))
+    {
+    case 1:
+      tuning_step = TUNING_STEP_160M;
+      break;
+    case 3:
+      tuning_step = TUNING_STEP_80M;
+      break;
+    case 5:
+      tuning_step = TUNING_STEP_60M;
+      break;
+    case 7:
+      tuning_step = TUNING_STEP_40M;
+      break;
+    case 10:
+      tuning_step = TUNING_STEP_30M;
+      break;
+    case 14:
+      tuning_step = TUNING_STEP_20M;
+      break;
+    case 18:
+      tuning_step = TUNING_STEP_18M;
+      break;
+    case 21:
+      tuning_step = TUNING_STEP_15M;
+      break;
+    case 24:
+      tuning_step = TUNING_STEP_12M;
+      break;
+    case 30:
+      tuning_step = TUNING_STEP_10M;
+      break;
+    default:
+      break;
+    }
     float swr = getSWR();
+    float swr_old = swr;
+    grid_vector.clear();
     if(DEBUG) {
-      char s [128];
-      sprintf (s, "[FRQ]: %4.2fMHz SWR: %2.2f", frequency, swr);
+      swr = getSWR();
+      char s[128] = {0};
+      char str_swr[7];
+      char str_frq[8];
+      dtostrf(frequency, 4, 2, str_frq);
+      dtostrf(swr, 2, 2, str_swr);
+      sprintf(s, "[FRQ]: %sMHz SWR: %s", str_frq, str_swr);
       Serial.println(s);
     }
-    if(swr > MAX_SWR) {
+    if((swr > MAX_SWR) && (swr < MAX_TUNING_SWR)) {
       tuned = false;
       reset_LC();
-      coarse_ind();
-      coarse_cap();
+      coarse_ind(tuning_step);
+      coarse_cap(tuning_step);
+      swr = getSWR();
+      if((swr >= swr_old) || (swr > MAX_SWR)) {
+        c_side_ant = LOW;
+        c_side_trx = HIGH;
+        clearRegistersC();
+        writeRegistersC();
+        coarse_cap(tuning_step);
+      }
       Serial.println("Fine tune...");
       for(int i = 3; i > 0; i--) {
-        for(int y = 0; y < 16; y++) {
-          fine_tuning(i);
-          if(tuned) { break; }
+        for(int y = 0; y < 3; y++) {
+          fine_tuning(i); // one last pass to finetune
+          if(tuned) { break; } 
         }
         if(tuned) { break; }
       }
-      delay(4000);
-      if(getSWR() > MAX_SWR) {
-        c_side_ant = LOW;
-        c_side_trx = HIGH;
-        writeRegistersC();
-        coarse_cap();
-      }
-      delay(3000);
+      delay(2000);
     }
   }
 }
